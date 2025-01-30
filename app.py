1a91ce65edb72a05c6fc7c081dcfc9247f385d2b
DENIS BOLLO <100396801+Bolodenis@users.noreply.github.com>
2025-01-30 11:11:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 from datetime import datetime
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/", methods=["GET"])
 def get_info():
     return jsonify({
-        "email": "bollodenis.com",
+        "email": "bollodenis@gmail.com",
         "current_datetime": datetime.utcnow().isoformat() + "Z",
-        "github_url": "https://github.com/Bolodenis/hgn_internship_task0"
+        "github_url": "https://github.com/Bolodenis/hgn_task0"
     })
 
 if __name__ == "__main__":

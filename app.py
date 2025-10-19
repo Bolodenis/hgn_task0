a394992354b50c38983d68ae317a6bfa0945c365
DENIS BOLLO <100396801+Bolodenis@users.noreply.github.com>
2025-10-19 21:46:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ def get_profile():
     return jsonify(result), 200  # Return JSON with HTTP 200 OK
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=7296)
+
 
 

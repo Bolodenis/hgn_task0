c523249f69677770a7c084788fa805f2deed2c1f
DENIS BOLLO <100396801+Bolodenis@users.noreply.github.com>
2025-10-19 21:03:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
+
 from datetime import datetime, timezone
 import requests
 
 app = Flask(__name__)
-CORS(app)
+
 
 @app.route("/me", methods=["GET"])
 def get_profile():
